f767cfb6a028f94530b0b7759333f1abc296d591
SelkaChan <eduardochineacl@gmail.com>
2023-02-10 15:17:03 +0000

    SuperMapa finished

--- SuperMapa.js
+++ SuperMapa.js
@@ -32,24 +32,42 @@ class SuperMapa extends HTMLElement{
   set precision(p) {
     this.setAttribute('precision', p);
   }
+
+  get lat() {
+    return this.getAttribute('lat');
+  }
+
+  set lat(l) {
+    this.setAttribute('lat', l);
+  }
+
+  get long() {
+    return this.getAttribute('long');
+  }
+
+  set long(l) {
+    this.setAttribute('long', l);
+  }
   static get observedAttributes() {
-    return ['ancho','alto','precision'];
+    return ['ancho','alto','precision','lat','long'];
   }
 
   attributeChangedCallback(name, old, now) {
     let supermapa = document.querySelector('super-mapa')
     let mapa = document.querySelector('iframe') || document.createElement('iframe');
+    mapa.width = this.ancho;
+    mapa.height = this.alto;
 
     switch (name) {
-      case 'ancho':
-        mapa.width = now;
-        break;
-        case 'alto':
-          mapa.height = now;
-          break;
         case 'precision':
           posicionar(this.lat,this.long,this.precision);
-          break;
+        break;
+        case 'lat':
+          posicionar(this.lat,this.long,this.precision);
+        break;
+        case 'long':
+          posicionar(this.lat,this.long,this.precision);
+        break;    
         }
         
       navigator.geolocation.watchPosition((position) => {
@@ -73,9 +91,26 @@ function desampliar() {
   supermapa.precision = parseFloat(supermapa.precision)*10; 
 }
 
+function subir() {
+  supermapa.lat= parseFloat(supermapa.lat) + 1;
+}
+
+function bajar() {
+  supermapa.lat= parseFloat(supermapa.lat) - 1;
+}
+
+function izquierda() {
+  supermapa.long= parseFloat(supermapa.long) - 1;
+}
+
+function derecha() {
+  supermapa.long= parseFloat(supermapa.long) + 1;
+}
+
+
 function posicionar(lat,long,pres){
   let mapa = document.querySelector('iframe')
-  mapa.src = `https://www.openstreetmap.org/export/embed.html?bbox=${long-parseFloat(pres)},${lat-parseFloat(pres)},${long+parseFloat(pres)},${lat+parseFloat(pres)}&layer=mapnik`
+  mapa.src = `https://www.openstreetmap.org/export/embed.html?bbox=${parseFloat(long)-parseFloat(pres)},${parseFloat(lat)-parseFloat(pres)},${parseFloat(long)+parseFloat(pres)},${parseFloat(lat)+parseFloat(pres)}&layer=mapnik`
 }
 
 
